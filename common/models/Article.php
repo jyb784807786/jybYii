efcac1ac03cd58591b52154f3271d2660d69ee01
784807786@qq.com <784807786@qq.com>
2017-03-12 23:18:54 +0800

    170312

---
+++ common/models/Article.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $content
+ * @property integer $created
+ * @property integer $updated
+ * @property integer $status
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'created', 'updated'], 'required'],
+            [['content'], 'string'],
+            [['created', 'updated', 'status'], 'integer'],
+            [['title'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+            'created' => 'Created',
+            'updated' => 'Updated',
+            'status' => 'Status',
+        ];
+    }
+}
